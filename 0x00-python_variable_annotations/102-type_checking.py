77a3934be093ffba23d36a500424da93423a8c9e
G-Merich <gmerit2030@gmail.com>
2023-08-03 21:26:00 +0000

    task

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,25 @@
-
 #!/usr/bin/env python3
-''' module for task 12 '''
-import typing
+""" Type Checking """
+from typing import Tuple, List
 
 
-def zoom_array(lst: typing.Tuple, factor: int = 2) -> typing.List:
-    ''' function to zoom array '''
-    zoomed_in: typing.List = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Parameters
+    ----------
+    lst: Tuple
+    factor: int
+
+    Return: List
+    """
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array: typing.Tuple = (12, 72, 91)
+array = tuple([12, 72, 91])
 
 zoom_2x = zoom_array(array)
 
